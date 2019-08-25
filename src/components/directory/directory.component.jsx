e1904fd9c16b8112f4ca6b3ba77925c848f62db5
DESKTOP-O1J837U\talbo <talboosy@gmail.com>
2019-08-25 20:52:22 +0300

    adding routing

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,29 +11,34 @@ class Directory extends React.Component {
                 {
                     title: 'hats',
                     imageUrl: 'https://images.pexels.com/photos/2663411/pexels-photo-2663411.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-                    id: 1
+                    id: 1,
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
                     imageUrl: 'https://images.pexels.com/photos/2838497/pexels-photo-2838497.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
-                    id: 2
+                    id: 2,
+                    linkUrl: ''
                 },
                 {
                     title: 'sneakers',
                     imageUrl: 'https://www.ncronline.org/sites/default/files/styles/article_full_width/public/lefteris-kallergis-j1GiPlvSGWI-unsplash.jpg?itok=PBMC3-PL',
-                    id: 3
+                    id: 3,
+                    linkUrl:''
                 },
                 {
                     title: 'womens',
                     imageUrl: 'https://images.pexels.com/photos/2823668/pexels-photo-2823668.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                     size: 'large',
-                    id: 4
+                    id: 4,
+                    linkUrl:''
                 },
                 {
                     title: 'mens',
                     imageUrl: 'https://images.pexels.com/photos/2657955/pexels-photo-2657955.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
                     size: 'large',
-                    id: 5
+                    id: 5,
+                    linkUrl: ''
                 }
         
         ]
@@ -44,8 +49,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     )
 
                     )
