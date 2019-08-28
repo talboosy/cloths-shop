bc66e3bd1d58623d75d069a1b31aa1560f9b6070
DESKTOP-O1J837U\talbo <talboosy@gmail.com>
2019-08-28 23:08:20 +0300

    adding sign up

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 import './auth.styles.scss'
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
 
 const Auth = () => (
     <div className='sign-in-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
