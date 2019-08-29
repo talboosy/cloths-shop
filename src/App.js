857b86d7c756f39bb226ce066b282ee52105e702
DESKTOP-O1J837U\talbo <talboosy@gmail.com>
2019-08-29 18:48:09 +0300

    adding redux and added userAction and userReducer to header and app component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import Auth from './pages/auth/auth.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null
 
   componentDidMount(){
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
         if (userAuth) {
           const userRef = await createUserProfileDocument(userAuth);
-
           userRef.onSnapshot(snapShot => {
-              this.setState({
-                currentUser: {
+              setCurrentUser({
                   id: snapShot.id,
                   ...snapShot.data()
-                }
-              });
+                })
           });
           
         }
-        this.setState({currentUser: userAuth})
+        setCurrentUser(userAuth)
     })
   }
 
@@ -44,7 +37,7 @@ class App extends React.Component {
   render(){
   return (
     <div>
-      <Header currentUser={this.state.currentUser }/>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
@@ -55,4 +48,9 @@ class App extends React.Component {
   );}
 }
 
-export default App;
+const mapDispathToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+
+export default connect(null, mapDispathToProps)(App);
