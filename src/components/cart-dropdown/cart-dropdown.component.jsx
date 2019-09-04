eea456c9a33406466b253cf275a552b22b53082b
Tal Boosy <tal@allegro.ai>
2019-09-04 22:21:14 +0300

    modifying checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,9 +8,9 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors'
 import { createStructuredSelector } from 'reselect';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js'
 
-
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
         <div className="cart-items">
             {
@@ -20,7 +20,10 @@ const CartDropdown = ({cartItems, history}) => (
                 <span className="empty-message">Your cart is empty!</span>
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT </CustomButton> 
+        <CustomButton onClick={() => {
+            history.push('/checkout'); 
+            dispatch(toggleCartHidden());
+            }}>GO TO CHECKOUT </CustomButton> 
     </div>
 )
 
