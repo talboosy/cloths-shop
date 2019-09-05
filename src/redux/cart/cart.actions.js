c08418dc37a0d3401c636938cbad5f2e643b0599
DESKTOP-O1J837U\talbo <talboosy@gmail.com>
2019-09-05 11:37:26 +0300

    adding remove item action

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,6 +7,11 @@ export const toggleCartHidden = () => ({
 export const addItem = item => ({
     type: CartActionTypes.ADD_ITEM,
     payload: item
+});
+
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
 })
 
 export const clearItemFromCart = item => ({
